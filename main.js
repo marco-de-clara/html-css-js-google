77f0a63684ed13f2e00cb1301ffff3fd7d4d35f8
Marco De Clara <marcodeclara@gmail.com>
2020-05-06 08:37:50 +0200

    Added dropdown menu

---
+++ main.js
@@ -0,0 +1,13 @@
+// catch click on menu icon to toggle dropdown menu
+$('.menu').click(function() {
+    $('.dropmenu').toggleClass('show');
+});
+
+// catch click outside of menu to close dropdown menu
+$(document).mouseup(function(event) {
+    // if the target is neither the dropdown menu, nor the menu icon, nor a descendant
+    if ( !$('.dropmenu').is(event.target) && !$('.menu').is(event.target) && $('.dropmenu').has(event.target).length == 0 ) {
+        // remove show class from dropdown menu
+        $('.dropmenu').removeClass('show');
+    }
+});
